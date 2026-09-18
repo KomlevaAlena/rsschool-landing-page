3af5e68dc4f7c269eb69831fc23d25a00daaf0e5
KomlevaAlena <norgiss404@gmail.com>
2026-09-19 03:38:10 +0500

    feat: testing docs folder

---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+// import mobileNav from './modules/mobile-nav.js';
+// import {initFilters} from './modules/card-filter.js';
+// import {initModal} from './modules/modal-card.js';
+// import {slider} from './modules/slider.js';
+// import {timer} from './modules/cta.js';
+// import {initNavigation} from './modules/smooth-scroll.js';
+// import {scrollToTop} from './modules/to-top.js';
+// import {renderBestGifts} from './modules/gifts-data.js';
+
+// window.addEventListener('DOMContentLoaded', () => {
+
+//   window.addEventListener('load', () => {
+//     mobileNav();
+//     initFilters();
+//     slider();
+//     timer();
+//     initNavigation();
+//     scrollToTop();
+//     renderBestGifts();
+//     if (document.querySelector('.gift-shopwindow__list')) {
+//         initModal();
+//     }
+//   });
+// });
